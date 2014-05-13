db7f3e88f6ff6767fe9cfcd05235c7330831c749
jaumemayol <jaumemayol@INFO3EQUIP17>
2014-05-13 20:58:23 +0200

    hjhj

--- SWPROTematStreams/src/cat/iespaucasesnoves/swpro/streams/ProvesByteStream.java
+++ SWPROTematStreams/src/cat/iespaucasesnoves/swpro/streams/ProvesByteStream.java
@@ -2,28 +2,67 @@ package cat.iespaucasesnoves.swpro.streams;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public abstract class ProvesByteStream {
 
 	// mostraBytes. Rep com a paràmetre un String amb la ruta d'un fitxer.
 	// Llegeix el fitxer byte a byte i el mostra per consola.
-	
-	public static void mostraBytes(String ruta) throws IOException{
+	public static void mostraBytes(String ruta) throws IOException {
 		try (FileInputStream in = new FileInputStream(ruta)) {
 			int c;
 			while ((c = in.read()) != -1) {
-			char d = (char) c;
-			System.out.print(d);
+				char d = (char) c;
+				System.out.print(d);
 			}
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-}
+
+	// escriuByte(String desti, String dades) Guarda al fitxer destí el text
+	// contingut dins dades.
+	public static void escriuByte(String desti, String dades)
+			throws IOException {
+		try (FileInputStream in = new FileInputStream(dades)) {
+			int c;
+			while ((c = in.read()) != -1) {
+				char d = (char) c;
+				FileOutputStream out = new FileOutputStream(desti);
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	// copiaBytes/String origen, String desti) . Copia dins el fitxer destí el
+	// contingut del fitxer origen. Prova-ho amb una imatge que tenguis a
+	// l'ordinador.
+	public static void copiaBytes(String origen, String desti)
+			throws IOException {
+		try (FileInputStream in = new FileInputStream(origen)) {
+			int c;
+			while ((c = in.read()) != -1) {
+				FileOutputStream out = new FileOutputStream(desti);
+				out.write(c);
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+public static void main(String[] args) throws IOException {
+		mostraBytes("C:/home/alumne/Escriptori/lorem.txt");
 		
+		
+	}
 	
 
-
+}
--- SWPROTematStreams/src/cat/iespaucasesnoves/swpro/streams/ProvesStreams.java
+++ SWPROTematStreams/src/cat/iespaucasesnoves/swpro/streams/ProvesStreams.java
@@ -2,34 +2,24 @@ package cat.iespaucasesnoves.swpro.streams;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Scanner;
 
 public class ProvesStreams {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+	
+		
+		
+		
+		
+	}
+}
+	
 
-        Scanner sc = new Scanner(System.in);
-        PrintStream salida;
-		try {
-            String cadena;
-            in = new FileInputStream("c:/ficheros/datos.txt");
-            System.out.println("Introduce texto. Para acabar introduce la cadena FIN:");
-            cadena = sc.nextLine();
-            while ((c = in.read()) != -1) {
-                salida.println(cadena);
-                cadena = sc.nextLine();
-            }
-            salida.flush();
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        } finally {         
-        	if (in != null) {
-        		in.close();
-        		}
-        }
-    }
-	ç
+    
+
 
 
