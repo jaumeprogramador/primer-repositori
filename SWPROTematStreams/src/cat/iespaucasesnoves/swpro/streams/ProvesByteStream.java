3f1aa7d70aee4c2efd4dc7896046d6d299e55658
jaumemayol <jaumemayol@INFO3EQUIP17>
2014-05-13 20:33:04 +0200

    mostraBytes

--- SWPROTematStreams/src/cat/iespaucasesnoves/swpro/streams/ProvesByteStream.java
+++ SWPROTematStreams/src/cat/iespaucasesnoves/swpro/streams/ProvesByteStream.java
@@ -1,5 +1,29 @@
 package cat.iespaucasesnoves.swpro.streams;
 
-public class ProvesByteStream {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+public abstract class ProvesByteStream {
+
+	// mostraBytes. Rep com a paràmetre un String amb la ruta d'un fitxer.
+	// Llegeix el fitxer byte a byte i el mostra per consola.
+	
+	public static void mostraBytes(String ruta) throws IOException{
+		try (FileInputStream in = new FileInputStream(ruta)) {
+			int c;
+			while ((c = in.read()) != -1) {
+			char d = (char) c;
+			System.out.print(d);
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
+		
+	
+
+
